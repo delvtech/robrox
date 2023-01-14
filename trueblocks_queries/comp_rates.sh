aae48c2d17e1d9ceddf3c2047a9d46831c3f931a
Mihai <mcosma@gmail.com>
2023-01-14 11:49:41 -0500

    all rates

--- trueblocks_queries/comp_rates.sh
+++ trueblocks_queries/comp_rates.sh
@@ -1,8 +1,30 @@
-#!/bin/bash
-last_month_block=$(chifra when 2022-12-13 --no_header | cut -f1)
+#!/bin/zsh
 
-chifra list 0x4Ddc2D193948926D02f9B1fE9e1daa0718270ED5 --no_header --first_block $last_month_block | cut -f2 | sort -u -n -r > last_month_blocks.txt
+# store compound cToken addresses
+eth=0x4Ddc2D193948926D02f9B1fE9e1daa0718270ED5
+btc=0xccF4429DB6322D5C611ee964527D42E5d685DD6a
 
-chifra state --call "0x4Ddc2D193948926D02f9B1fE9e1daa0718270ED5 | supplyRatePerBlock()(uint)" --file last_month_blocks.txt --no_header | awk '$5 ~ /^[1-9]/ { print $1","(($5 / 10^18 * 7200 * 365 * 100)) }' > month_results.txt
+# export all appearances
+chifra list $eth --no_header | cut -f2 | sort -u -n -r >blocksETH.txt
+chifra list $btc --no_header | cut -f2 | sort -u -n -r >blocksBTC.txt
 
-< month_results.txt jp -input csv -xy '[*][0,1]'
+# export all rates
+chifra state --call "$eth | supplyRatePerBlock()(uint)" --file blocksETH.txt --no_header | awk '$5 ~ /^[1-9]/ { print (($5 / 10^18 * 7200 * 365)) }' >resultsETH.txt
+chifra state --call "$btc | supplyRatePerBlock()(uint)" --file blocksBTC.txt --no_header | awk '$5 ~ /^[1-9]/ { print (($5 / 10^18 * 7200 * 365)) }' >resultsBTC.txt
+
+# count the number of result lines
+# this is less than the number of blocks
+# because chifra ignores "unripe" blocks
+# more than 28 blocks behind the head
+result_lines_eth=$(wc -l resultsETH.txt | cut -f1 -d ' ')
+result_lines_btc=$(wc -l resultsBTC.txt | cut -f1 -d ' ')
+
+# concatenate the results
+tail -n $result_lines_eth blocksETH.txt >blocksETHwithresults.txt
+tail -n $result_lines_btc blocksBTC.txt >blocksBTCwithresults.txt
+paste -d ',' blocksETHwithresults.txt resultsETH.txt >combinedETH.txt
+paste -d ',' blocksBTCwithresults.txt resultsBTC.txt >combinedBTC.txt
+
+# put on power glove and display in terminal
+jp <combinedETH.txt --input csv -xy '[*][0,1]'
+jp <combinedBTC.txt --input csv -xy '[*][0,1]'
